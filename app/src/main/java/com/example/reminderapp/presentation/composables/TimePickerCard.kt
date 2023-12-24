810d2d09dd96448e3f69a5e3c3a85ed2e13fa36d
Zaur Aslakhanov <z.asla@hotmail.com>
2023-12-24 20:54:10 +0100

    #5: Fixes timepicker dialog

--- app/src/main/java/com/example/reminderapp/presentation/composables/TimePickerCard.kt
+++ app/src/main/java/com/example/reminderapp/presentation/composables/TimePickerCard.kt
@@ -1,11 +1,13 @@
 package com.example.reminderapp.presentation.composables
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -21,12 +23,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.reminderapp.R
 import com.example.reminderapp.presentation.ReminderIntent
-import com.example.reminderapp.presentation.ReminderState
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
@@ -91,39 +95,61 @@ fun TimePickerDialog(
 
     androidx.compose.material3.AlertDialog(
         modifier = Modifier
-            .fillMaxWidth(),
-        onDismissRequest = { onDismissRequest() }
-    ) {
-        Column(
-            modifier = Modifier
-                .background(
-                    color = Color.White.copy(alpha = 0.4F)
-                )
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TimeInput(state = timePickerState)
-            Row(
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp)),
+        onDismissRequest = { onDismissRequest() },
+        content = {
+            Column(
                 modifier = Modifier
+                    .background(color = Color.White)
                     .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Dismiss button
-                TextButton(onClick = { onDismissRequest() }) {
-                    Text(text = "Cancel")
-                }
-                // Confirm button
-                TextButton(
-                    onClick = {
-                        onTimeSelected(LocalTime.of(timePickerState.hour, timePickerState.minute))
-                        onDismissRequest()
-                    }) {
-                    Text(text = "Confirm")
+                TimeInput(modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp),
+                    state = timePickerState
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    // Dismiss button
+                    TextButton(onClick = { onDismissRequest() }) {
+                        Text(
+                            text = "Cancel",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    // Confirm button
+                    TextButton(
+                        onClick = {
+                            onTimeSelected(LocalTime.of(timePickerState.hour, timePickerState.minute))
+                            onDismissRequest()
+                        }) {
+                        Text(
+                            text = "Confirm",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
         }
-    }
+    )
 }
 
+
+
+@Preview
+@Composable
+fun TimePickerDialogPreview() {
+    var showTimePickerDialog by remember {
+        mutableStateOf(false)
+    }
+    TimePickerDialog({ Log.d("TAG", "HEllo")},{ showTimePickerDialog = false } )
+}
